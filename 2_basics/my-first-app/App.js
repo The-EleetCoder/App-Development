1b8758bc7a9f75c980eb64d6044d86cac84ba858
The-EleetCoder <jaijain1803@gmail.com>
2023-12-26 15:20:26 +0530

    day 2

--- 2_basics/my-first-app/App.js
+++ 2_basics/my-first-app/App.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+  const addGoalHandler = () => {
+    console.log(enteredGoalText)
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Your course goal!"
+        <TextInput
+          placeholder="Your course goal"
           style={styles.textInput}
+          onChangeText={goalInputHandler}
         />
-        <Button title="Add Goal"/>
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -19,17 +30,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
-  textInput:{
+  textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
+    borderColor: "#cccccc",
+    width: "70%",
     marginRight: 8,
-    padding: 8
-  }
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
 });
