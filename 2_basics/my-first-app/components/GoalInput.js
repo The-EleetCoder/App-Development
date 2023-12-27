37b841b5c3710d0b2824d3adb51b90a300cf7242
The-EleetCoder <jaijain1803@gmail.com>
2023-12-27 12:28:36 +0530

    day 3

--- 2_basics/my-first-app/components/GoalInput.js
+++ 2_basics/my-first-app/components/GoalInput.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import {useState} from 'react'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
 
-const GoalInput = () => {
+const GoalInput = (props) => {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+const addGoalHandler = () => {
+  props.onAddGoal(enteredGoalText);
+  setEnteredGoalText("");
+}
+
   return (
-    <div>GoalInput</div>
+    <View style={styles.inputContainer}>
+    <TextInput
+      placeholder="Your course goal"
+      style={styles.textInput}
+      onChangeText={goalInputHandler}
+      value={enteredGoalText}
+    />
+    <Button title="Add Goal" onPress={addGoalHandler} />
+  </View>
   )
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  }
+})
 
 export default GoalInput
