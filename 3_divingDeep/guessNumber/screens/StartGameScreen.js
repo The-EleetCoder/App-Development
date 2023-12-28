a7ef0e90439c7ab0da2a19dd5fb231a972ea799c
The-EleetCoder <jaijain1803@gmail.com>
2023-12-28 13:01:02 +0530

    day 4

--- 3_divingDeep/guessNumber/screens/StartGameScreen.js
+++ 3_divingDeep/guessNumber/screens/StartGameScreen.js
@@ -1,10 +1,29 @@
-import { Text } from "react-native"
-
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton.js";
 
 const StartGameScreen = () => {
   return (
-    <Text>StartGameScreen</Text>
-  )
-}
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm.</PrimaryButton>
+    </View>
+  );
+};
 
-export default StartGameScreen
+const styles = StyleSheet.create({
+  inputContainer:{
+    padding: 16,
+    marginTop:100,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    marginHorizontal: 24,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
+
+export default StartGameScreen;
