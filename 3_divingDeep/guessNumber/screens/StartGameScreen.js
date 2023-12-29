2081615887862cacd8f18ad2d75ddd34e8644311
The-EleetCoder <jaijain1803@gmail.com>
2023-12-29 12:16:45 +0530

    day 5

--- 3_divingDeep/guessNumber/screens/StartGameScreen.js
+++ 3_divingDeep/guessNumber/screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton.js";
 
-const StartGameScreen = () => {
+const StartGameScreen = ({onPickNumber}) => {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   const numberInputHandler = (enteredText) => {
@@ -22,6 +22,7 @@ const StartGameScreen = () => {
       ]);
       return;
     }
+    onPickNumber(chosenNumber);
   };
 
   return (
