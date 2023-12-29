d28b418f275277715e61c0115b51f95a16632a92
The-EleetCoder <jaijain1803@gmail.com>
2023-12-29 16:07:16 +0530

    day 5

--- 3_divingDeep/guessNumber/screens/GameScreen.js
+++ 3_divingDeep/guessNumber/screens/GameScreen.js
@@ -1,35 +1,40 @@
-import { Text, View, StyleSheet } from "react-native"
-import Title from "../components/Title"
+import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import { useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
 
 function generateRandomBetween(min, max, exclude) {
-    const rndNum = Math.floor(Math.random() * (max - min)) + min;
-  
-    if (rndNum === exclude) {
-      return generateRandomBetween(min, max, exclude);
-    } else {
-      return rndNum;
-    }
-  }
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
 
-const GameScreen = ({children}) => {
-  return (
-    <View style={styles.screen}>
-        <Title>Opponent's Guess</Title>
-        {/* Guess */}
-        <View>
-            <Text>Higher or Lower?</Text>
-            {/* + - */}
-        </View>
-        {/* <View>Log Rounds</View> */}
-    </View>
-  )
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
 }
 
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        padding: 12
-    },
-})
+const GameScreen = ({userNumber}) => {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
-export default GameScreen
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>Log Rounds</View> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
+
+export default GameScreen;
