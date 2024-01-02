3ea8f65454f48a3a53a68ba04a7e38bb559d58a6
The-EleetCoder <jaijain1803@gmail.com>
2024-01-02 13:02:16 +0530

    day 9

--- 4_navigation/mealsApp/components/MealItem.js
+++ 4_navigation/mealsApp/components/MealItem.js
@@ -7,19 +7,30 @@ import {
   Platform,
 } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function MealItem({
+  id,
   title,
   imageUrl,
   duration,
   complexity,
   affordability,
 }) {
+  const navigation = useNavigation();
+
+  const mealItemHandler = () => {
+    navigation.navigate("MealDetail", {
+      mealId: id,
+    });
+  };
+
   return (
     <View style={styles.mealItem}>
       <Pressable
         android_ripple={{ color: "#ccc" }}
         style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+        onPress={mealItemHandler}
       >
         <View>
           <View style={styles.innerContainer}>
---
+++ 4_navigation/mealsApp/screens/MealDetailScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MealDetailScreen({route}) {
+    const mealId = route.params.mealId
+  return (
+    <View>
+      <Text>MealDetailScreen {mealId}</Text>
+    </View>
+  )
+}
