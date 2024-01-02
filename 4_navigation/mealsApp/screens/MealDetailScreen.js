a5a39b4238db34e1d1e70107bc008fc45a1e69e2
The-EleetCoder <jaijain1803@gmail.com>
2024-01-02 17:34:55 +0530

    day 9

--- 4_navigation/mealsApp/screens/MealDetailScreen.js
+++ 4_navigation/mealsApp/screens/MealDetailScreen.js
@@ -5,6 +5,7 @@ import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import IconButton from "../components/IconButton";
 
 export default function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
@@ -17,7 +18,7 @@ export default function MealDetailScreen({ route, navigation }) {
   useLayoutEffect(()=>{
     navigation.setOptions({
       headerRight:()=>{
-        return <Button title="Tap me" onPress={headerButtonPressHandler}/>
+        return <IconButton onPress={headerButtonPressHandler} icon='star' color='white' />;
       }
     })
   },[navigation, headerButtonPressHandler]);
