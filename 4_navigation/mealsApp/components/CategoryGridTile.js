459ceff9882426445e282c1e0693131490ea6b87
The-EleetCoder <jaijain1803@gmail.com>
2024-01-02 10:45:27 +0530

    day 9

--- 4_navigation/mealsApp/components/CategoryGridTile.js
+++ 4_navigation/mealsApp/components/CategoryGridTile.js
@@ -1,11 +1,18 @@
 import { View, Text, Pressable, StyleSheet, Platform } from "react-native";
 import React from "react";
 
-export default function CategoryGridTile({ title, color }) {
+export default function CategoryGridTile({ title, color, onPress }) {
   return (
     <View style={[styles.gridItem]}>
-      <Pressable android_ripple={{color: '#ccc'}} style={({pressed})=>[styles.button, pressed ? styles.buttonPressed : null]}>
-        <View style={[styles.innerContainer,  {backgroundColor: color}]}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        onPress={onPress}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
@@ -14,35 +21,34 @@ export default function CategoryGridTile({ title, color }) {
 }
 
 const styles = StyleSheet.create({
-  gridItem:{
-    flex:1,
+  gridItem: {
+    flex: 1,
     margin: 16,
     height: 150,
     borderRadius: 8,
     elevation: 4,
-    backgroundColor: 'white',
-    shadowColor: 'black',
+    backgroundColor: "white",
+    shadowColor: "black",
     shadowOpacity: 0.25,
-    shadowOffset: {width: 0, height: 2},
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
-    overflow: Platform.OS === 'android' ? 'hidden' : 'vidible'
+    overflow: Platform.OS === "android" ? "hidden" : "vidible",
   },
-  button:{
+  button: {
     flex: 1,
   },
-  buttonPressed:{
+  buttonPressed: {
     opacity: 0.5,
   },
-  innerContainer:{
+  innerContainer: {
     flex: 1,
-    padding:16,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 8,
-
   },
-  title:{
-    fontWeight: 'bold',
-    fontSize: 18
-  }
-})
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
---
+++ 4_navigation/mealsApp/screens/MealsOverviewScreen.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native'
+import { MEALS } from '../data/dummy-data'
+import React from 'react'
+
+export default function MealsOverviewScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>MealsOverviewScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    }
+})
