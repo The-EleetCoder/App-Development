6b0260cf83d2ef4b43a60f9651108b41d928d0cc
The-EleetCoder <jaijain1803@gmail.com>
2024-01-03 11:21:08 +0530

    day 10

--- 4_navigation/navigationApp/App.js
+++ 4_navigation/navigationApp/App.js
@@ -1,15 +1,16 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import UserScreen from './screens/UserScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="User">
+      <Drawer.Navigator>
         <Drawer.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Drawer.Screen name="User" component={UserScreen} />
       </Drawer.Navigator>
