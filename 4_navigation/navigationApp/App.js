4083c771a4c1d8dbeca31b9953e2be38dc07697a
The-EleetCoder <jaijain1803@gmail.com>
2024-01-03 11:50:06 +0530

    day 10

--- 4_navigation/navigationApp/App.js
+++ 4_navigation/navigationApp/App.js
@@ -1,18 +1,35 @@
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import UserScreen from './screens/UserScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import UserScreen from "./screens/UserScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import { Ionicons } from '@expo/vector-icons';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="WelcomeScreen" component={WelcomeScreen} />
-        <Drawer.Screen name="User" component={UserScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          drawerActiveBackgroundColor: "#f0e1ff",
+          drawerActiveTintColor: "#3c0a6b",
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+        }}
+      >
+        <Drawer.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({color, size}) => <Ionicons name="home" color={color} size={size} />
+          }}
+        />
+        <Drawer.Screen name="User" component={UserScreen}  options={{
+            drawerIcon: ({color, size}) => <Ionicons name="person" color={color} size={size} />
+          }} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
@@ -21,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
