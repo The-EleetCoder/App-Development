7ea37503f37659c337318e053a66c8093a8c75dd
The-EleetCoder <jaijain1803@gmail.com>
2024-01-04 16:10:42 +0530

    day 11

--- 5_ExpenseTrackerApp/screens/ManageExpense.js
+++ 5_ExpenseTrackerApp/screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { GlobalStyles } from "../constants/styles";
 import IconButton from "../components/UI/IconButton";
+import Button from "../components/UI/Button";
 
 export default function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -15,8 +16,18 @@ export default function ManageExpense({ route, navigation }) {
 
   function deleteExpenseHandler() {}
 
+  function cancelHandler() {}
+
+  function confirmHandler() {}
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button mode="flat" onPress={cancelHandler} style={styles.button}>
+          Cancel
+        </Button>
+        <Button onPress={confirmHandler} style={styles.button} >{isEditing ? 'Update' : 'Add'}</Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -36,6 +47,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button:{
+    minWidth: 120,
+    marginHorizontal: 8
   },
   deleteContainer: {
     marginTop: 16,
