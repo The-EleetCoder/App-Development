c988377211c7715a58863afaf1f0ebd8605c9283
The-EleetCoder <jaijain1803@gmail.com>
2024-01-05 16:57:19 +0530

    day 12

--- 5_ExpenseTrackerApp/components/ManageExpense/ExpenseForm.js
+++ 5_ExpenseTrackerApp/components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import React, { useState } from "react";
 import Input from "./Input";
 import Button from "../UI/Button";
@@ -25,6 +25,15 @@ export default function ExpenseForm({ submitButtonLabel, onCancel, onSubmit, def
       amount: +inputValues.amount,
       date: new Date(inputValues.date),
       description: inputValues.description
+    }
+
+    const amountIsvalid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if ( !amountIsvalid || !dateIsValid || !descriptionIsValid ) {
+      Alert.alert('Invalid input', 'Please check your input values');
+      return;
     }
 
     onSubmit(expenseData);
