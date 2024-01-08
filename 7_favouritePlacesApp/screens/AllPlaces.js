60cdbb49b25fe8c8783dce0a83ad95b8a9039171
The-EleetCoder <jaijain1803@gmail.com>
2024-01-08 16:55:21 +0530

    day 15

---
+++ 7_favouritePlacesApp/screens/AllPlaces.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import PlacesList from '../components/Places/PlacesList'
+
+export default function AllPlaces() {
+  return (
+    <PlacesList/>
+  )
+}
