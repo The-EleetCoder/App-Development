0aa35ddfc1f672b81f197aa13e21ceffaf0f60bb
The-EleetCoder <jaijain1803@gmail.com>
2024-01-08 13:46:57 +0530

    day 15

--- 6_userAuthentication/screens/LoginScreen.js
+++ 6_userAuthentication/screens/LoginScreen.js
@@ -1,15 +1,20 @@
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from "../components/ui/LoadingOverlay";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { login } from '../util/auth';
 import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authCtx.authenticate(token);
     }
     catch (err){
       Alert.alert('Authentication Failed!', 'Could not log you in. Please check your credentials and try again later.');
---
+++ 6_userAuthentication/store/auth-context.js
@@ -0,0 +1,31 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext({
+  token: "",
+  isAuthenticated: false,
+  authenticate: (token) => {},
+  logout: () => {},
+});
+
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthtoken] = useState();
+
+  function authenticate(token) {
+    setAuthtoken(token);
+  }
+
+  function logout() {
+    setAuthtoken(null);
+  }
+
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContextProvider;
