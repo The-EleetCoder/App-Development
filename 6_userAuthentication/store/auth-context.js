f1369e4759b286be73c9056169b1d3df7a15647d
The-EleetCoder <jaijain1803@gmail.com>
2024-01-08 14:57:11 +0530

    day 15

--- 6_userAuthentication/store/auth-context.js
+++ 6_userAuthentication/store/auth-context.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createContext, useState } from "react";
 
 export const AuthContext = createContext({
@@ -12,10 +13,12 @@ function AuthContextProvider({ children }) {
 
   function authenticate(token) {
     setAuthtoken(token);
+    AsyncStorage.setItem("token", token);
   }
 
   function logout() {
     setAuthtoken(null);
+    AsyncStorage.removeItem("token");
   }
 
   const value = {
